c0e3c71330fc910be56fe885a7c0c632977b7134
Igor <igorlfs@ufmg.br>
2023-05-16 13:59:06 -0300

    4: leia do arquivo

--- 4-caixeiro-viajante/main.py
+++ 4-caixeiro-viajante/main.py
@@ -2,13 +2,11 @@
 from typing import cast
 
 import matplotlib.pyplot as plt
-import mplcatppuccin  # noqa
-import mplcatppuccin.palette as cat
 import numpy as np
 from numba import jit
 from numpy.typing import NDArray
 
-# |%%--%%| <jFFsSsF762|5bIZbFwebX>
+# |%%--%%| <MrHJR5Y2F4|5bIZbFwebX>
 
 # Define as posições aleatórias das cidades
 rng = np.random.default_rng(seed=42)
@@ -104,12 +102,6 @@ def mc_step(
         + dist[new_path[right], new_path[fin]]
     )
 
-    # Estratégia do Rio
-    # if (x[new_path[left]] < RIVER and x[new_path[right]] >= RIVER) or (
-    #     x[new_path[left]] >= RIVER and x[new_path[right]] < RIVER
-    # ):
-    #     delta *= RIVER_COST
-
     if delta < 0:  # aplica o criterio de Metropolis
         energy += delta
         path = new_path
@@ -143,15 +135,14 @@ def generate_graph(num: np.int64) -> tuple[NDArray, NDArray, NDArray]:
 # |%%--%%| <M8kUzJF4dp|PmDkupE1CQ>
 
 
-def plot_path(x: NDArray, y: NDArray, path: NDArray, cost: float):
+def plot_path(x: NDArray, y: NDArray, path: NDArray, cost: float, num_cities: int):
     """Imprima o caminho encontrado."""
-    plt.plot(x, y, "o", color=blue)
     plt.title(f"Comprimento: {cost}")
-    for i in range(NUM_OF_CITIES):
+    for i in range(num_cities):
         plt.plot(
-            [x[path[i]], x[path[(i + 1) % NUM_OF_CITIES]]],
-            [y[path[i]], y[path[(i + 1) % NUM_OF_CITIES]]],
-            color=blue,
+            [x[path[i]], x[path[(i + 1) % num_cities]]],
+            [y[path[i]], y[path[(i + 1) % num_cities]]],
+            color="blue",
         )
     plt.show()
 
@@ -159,65 +150,22 @@ def plot_path(x: NDArray, y: NDArray, path: NDArray, cost: float):
 # |%%--%%| <PmDkupE1CQ|EMBIcvI8Kk>
 
 
-def plot_metrics(temperatures: NDArray, path_cost: NDArray):
+def plot_metrics(temperatures: NDArray, path_cost: NDArray, num_cities: int):
     """Imprima a evolução das temperaturas e do custo do caminho."""
     _, ax = plt.subplots()
     ax = cast(plt.Axes, ax)
-    plt.title(f"N: {NUM_OF_CITIES} $T_0$: {T_INIT} base: {MULTIPLIER}")
+    plt.title(f"N: {num_cities} $T_0$: {T_INIT} base: {MULTIPLIER}")
 
     ax2 = ax.twinx()
-    ax.plot(path_cost, color=blue)
-    ax2.plot(temperatures, color=red)
+    ax.plot(path_cost, color="blue")
+    ax2.plot(temperatures, color="red")
 
     ax.set_xlabel("Passos de Monte Carlo")
-    ax.set_ylabel("Custo", color=blue)
-    ax2.set_ylabel("Temperaturas", color=red)
+    ax.set_ylabel("Custo", color="blue")
+    ax2.set_ylabel("Temperaturas", color="red")
 
 
-# |%%--%%| <EMBIcvI8Kk|XFEn7uQvps>
-
-plt.style.use("mocha")
-
-# |%%--%%| <XFEn7uQvps|qj2Zna6kbj>
-
-blue = cat.load_color("mocha", "blue")
-red = cat.load_color("mocha", "red")
-
-# |%%--%%| <qj2Zna6kbj|UzHUFr8dyQ>
-
-NUM_OF_CITIES = np.int64(150)
-MC_STEPS = 30000
-T_INIT = 2.0 * NUM_OF_CITIES
-MULTIPLIER = 0.99999
-
-# |%%--%%| <UzHUFr8dyQ|WwBHAgXrK2>
-
-x, y, path = generate_graph(NUM_OF_CITIES)
-
-# |%%--%%| <WwBHAgXrK2|nTysyxQZ3s>
-
-path_cost = np.zeros(MC_STEPS, dtype=np.float64)
-temperatures = np.zeros(MC_STEPS, dtype=np.float64)
-beta = np.float64(T_INIT)
-dist = calculate_distance(NUM_OF_CITIES, x, y)
-cost = calculate_cost(NUM_OF_CITIES, path, dist)
-best_e = cost
-best_p = path
-
-for i in range(MC_STEPS):
-    cost, path, best_e, best_p = mc_step(
-        NUM_OF_CITIES, beta, cost, path, best_e, best_p, dist
-    )
-    path_cost[i] = best_e
-    temperatures[i] = beta
-    beta = beta * MULTIPLIER
-
-# |%%--%%| <nTysyxQZ3s|CcDL0TMWst>
-
-plot_path(x, y, best_p, best_e)
-plot_metrics(temperatures, path_cost)
-
-# |%%--%%| <CcDL0TMWst|6Y9DnasT0B>
+# |%%--%%| <EMBIcvI8Kk|6Y9DnasT0B>
 r"""°°°
 ## Análise
 
@@ -225,9 +173,47 @@ O método de simulated annealing é razoável, basta fazer o tuning correto dos 
 °°°"""
 # |%%--%%| <6Y9DnasT0B|zl6jNTwcRe>
 
-filename = input()
-file = np.loadtxt(filename)
-x, y = file[0], file[1]
+file = np.loadtxt("posicoes.dat")
+x, y = file[:, 0], file[:, 1]
 path = np.zeros(len(x), dtype=np.int64)
 for i in range(len(x)):
     path[i] = i
+
+# |%%--%%| <zl6jNTwcRe|wLMKZyeeLE>
+
+NUM_OF_CITIES = len(x)
+MC_STEPS = 30000
+T_INIT = 2.5 * NUM_OF_CITIES
+MULTIPLIER = 0.999999
+
+# |%%--%%| <wLMKZyeeLE|vd5zMy4ZaN>
+
+
+@jit(nopython=True)
+def main(path: NDArray[np.int64]):
+    """Função com o loop principal de otimização."""
+    path_cost = np.zeros(MC_STEPS, dtype=np.float64)
+    temperatures = np.zeros(MC_STEPS, dtype=np.float64)
+    beta = np.float64(T_INIT)
+    dist = calculate_distance(NUM_OF_CITIES, x, y)
+    cost = calculate_cost(NUM_OF_CITIES, path, dist)
+    best_e = cost
+    best_p = path
+
+    for i in range(MC_STEPS):
+        cost, path, best_e, best_p = mc_step(
+            NUM_OF_CITIES, beta, cost, path, best_e, best_p, dist
+        )
+        path_cost[i] = best_e
+        temperatures[i] = beta
+        beta = beta * MULTIPLIER
+    return best_p, best_e, path_cost, temperatures
+
+
+# |%%--%%| <vd5zMy4ZaN|CcDL0TMWst>
+
+best_p, best_e, path_cost, temperatures = main(path)
+plot_path(x, y, best_p, best_e, NUM_OF_CITIES)
+plot_metrics(temperatures, path_cost, NUM_OF_CITIES)
+
+# |%%--%%| <CcDL0TMWst|Ld0P5EEuQ1>
